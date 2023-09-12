a73cc739e6553d357b8fc42ceb76405d25fe1922
Arnold Supriyadi <arnoldsupriyadi@gmail.com>
2023-09-12 09:24:36 +0700

    first commit

---
+++ tests/Feature/HelloTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HelloTest extends TestCase
+{
+    public function testHello()
+    {
+        $this->get('/hello')
+            ->assertSeeText('Arnold');
+    }
+
+    public function testHelloWorld()
+    {
+        $this->get('/world')
+            ->assertSeeText('Arnold');
+    }
+
+    public function testHelloView()
+    {
+        $this->view('hello', ['name' => 'Arnold'])
+            ->assertSeeText('Arnold');
+    }
+
+    public function testHelloWorldView()
+    {
+        $this->view('hello.world', ['name' => 'Arnold'])
+            ->assertSeeText('Arnold');
+    }
+}
